cd17b9098f53c8bfcf47851cf4713b5f39cae66b
Ahemd Badr <ahmed.badr.abdeldayem.dev@gmail.com>
2025-03-14 15:40:41 +0200

    feat: Update WeatherRepository to include day count in weather forecast fetch
    This commit modifies the `WeatherRepository` interface by adding a `dayCount` parameter to the `fetchWeatherForecastByCityFromRemote` method.
    
    -   **domain/src/main/java/com/ab/domain/repository/WeatherRepository.kt:**
        -   Modified `fetchWeatherForecastByCityFromRemote` method to accept an additional `dayCount` parameter.

--- domain/src/main/java/com/ab/domain/repository/WeatherRepository.kt
+++ domain/src/main/java/com/ab/domain/repository/WeatherRepository.kt
@@ -3,6 +3,6 @@ package com.ab.domain.repository
 import com.ab.domain.model.dto.WeatherForecastResponseDto
 
 interface WeatherRepository {
-    suspend fun fetchWeatherForecastByCityFromRemote(cityName: String): WeatherForecastResponseDto
+    suspend fun fetchWeatherForecastByCityFromRemote(cityName: String, dayCount: Int): WeatherForecastResponseDto
 
 }

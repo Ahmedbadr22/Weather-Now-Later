ffbdb91d120ef39c9474bac7c08a68ba935e9386
Ahemd Badr <ahmed.badr.abdeldayem.dev@gmail.com>
2025-03-16 22:31:05 +0200

    feat: Add snow weather icon
    This commit adds a new weather icon for snowy conditions and updates the `WeatherIcons` class to include it.
    
    -   **WeatherUtils/src/main/java/com/ab/weatherutils/WeatherIcons.kt:**
        -   Added `snow` to the list of weather conditions recognized by `getWeatherIconRes`, mapping it to the new `ic_snow` drawable.
    -   **WeatherUtils/src/main/res/drawable/ic_snow.xml:**
        -   Created `ic_snow.xml` as a new vector drawable representing a snow icon.

--- WeatherUtils/src/main/java/com/ab/weatherutils/WeatherIcons.kt
+++ WeatherUtils/src/main/java/com/ab/weatherutils/WeatherIcons.kt
@@ -9,6 +9,7 @@ object WeatherIcons {
             "clear", "sunny", "sun" -> R.drawable.ic_sunny
             "cloudy", "clouds" -> R.drawable.ic_cloudy
             "rainy", "rain" -> R.drawable.ic_rainy
+            "snow" -> R.drawable.ic_snow
             else -> R.drawable.ic_unknown
         }
     }

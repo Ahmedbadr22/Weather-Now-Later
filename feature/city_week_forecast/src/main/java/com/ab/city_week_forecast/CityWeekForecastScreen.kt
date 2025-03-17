80508c8f5dc0130a8c3c77d766f5a980a52702e4
Ahemd Badr <ahmed.badr.abdeldayem.dev@gmail.com>
2025-03-17 11:48:48 +0200

    feat: Add localized "navigate back" string and implement it in CityWeekForecastScreen
    This commit introduces a localized string for the "navigate back" action and implements its usage in the `CityWeekForecastScreen`.
    
    -   **feature/city_week_forecast/src/main/res/values/strings.xml:**
        - Added a new `strings.xml` file to store the default localized string for "navigate back".
    -   **feature/city_week_forecast/src/main/res/values-ar/strings.xml:**
        - Added a new `strings.xml` file in the `values-ar` directory to store Arabic translation for "navigate back".
        - Added localized strings for "navigate back" in Arabic.
    -   **feature/city_week_forecast/src/main/java/com/ab/city_week_forecast/CityWeekForecastScreen.kt:**
        - Replaced the `null` content description for the back arrow `Icon` with a localized string resource.
        - Used `stringResource` to get the "navigate back" string.

--- feature/city_week_forecast/src/main/java/com/ab/city_week_forecast/CityWeekForecastScreen.kt
+++ feature/city_week_forecast/src/main/java/com/ab/city_week_forecast/CityWeekForecastScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ab.core.ui.theme.WeatherNowLaterTheme
@@ -73,7 +74,7 @@ fun CityWeekForecastScreen(
                     ) {
                         Icon(
                             Icons.AutoMirrored.Outlined.ArrowBack,
-                            contentDescription = null
+                            contentDescription = stringResource(com.ab.city_week_forecast.R.string.navigate_back)
                         )
                     }
                 }
